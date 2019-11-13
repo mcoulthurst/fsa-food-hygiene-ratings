9ac8163a793ab69e8807bcb7e733be834dfd7ee3
mcoulthurst <mc@lightness.co.uk>
2019-11-13 16:25:50 +0000

    Change search to distance from lat, long coords

--- app/routes.js
+++ app/routes.js
@@ -7,13 +7,15 @@ const _ = require('underscore');
 const API_URI = 'https://ratings.food.gov.uk/';
 
 
-var searchType = 'search-address';
+var searchType = 'enhanced-search'; // search, search-address, enhanced-search
 var nameStr = 'cafe';
 var addressStr = 'strutton ground';
 var addressStr = 'wilton road, london, sw1v';
 var postcode = 'SW1H';
 var format = 'json';
 var sortOrder = 'Alpha';
+var lat = -0.135; // 3dp = aprox 350 ft
+var long = 51.499;
 
 
 
@@ -27,10 +29,17 @@ router.get('/error', function(req, res, next) {
 });
 
 
+
 router.get('/results', function(req, res, next) {
-	var uri  = API_URI+'/'+searchType+'/'+addressStr+'/1/50/'+format+'/';
+  //var uri = API_URI+'/'+nameStr+'/'+addressStr+'/'+format+'/';
+
+  //https://ratings.food.gov.uk/enhanced-search/en-GB/^/^/DISTANCE/0/^/-4.73561176951173/55.9421692082746/1/30/xml
+  //https://ratings.food.gov.uk/enhanced-search/en-GB/^/^/DISTANCE/0/^-4.735/55.942/1/50/json/
+
+  //var uri = API_URI+'/'+searchType+'/'+addressStr+'/1/50/'+format+'/';
+	var uri = API_URI+searchType+'/en-GB/^/^/DISTANCE/0/^/'+lat+'/'+long+'/1/90/'+format+'/';
+
 	console.log(uri);
-    //request(API_URI+'/'+nameStr+'/'+addressStr+'/'+format+'/', {
     request(uri, {
       method: "GET",
       headers: {
@@ -43,6 +52,7 @@ router.get('/results', function(req, res, next) {
             if(body) {
               dataset = JSON.parse(body);
               var locations = dataset.FHRSEstablishment.EstablishmentCollection.EstablishmentDetail;
+              console.log(locations[0]);
 
               res.render('results', {
                 name: nameStr,
